a0111bcc1a5bb6197ac4f6e5765b804f74146607
Raj Dubey <com.mailuser@gmail.com>
2022-10-29 00:32:03 +0530

    fixed upload issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { MyFilesComponent } from './component/my-files/my-files.component';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +7,16 @@ import { AppComponent } from './app.component';
 import { UploadComponent } from './components/upload/upload.component';
 import { ViewFileComponent } from './components/view-file/view-file.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'upload', component: UploadComponent },
+  { path: '', redirectTo: '/upload', pathMatch: 'full'},
+  {path:'view',component:ViewFileComponent},
+  {path:'my-files',component:MyFilesComponent}
+];
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +27,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
