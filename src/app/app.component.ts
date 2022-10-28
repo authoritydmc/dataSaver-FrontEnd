65dd26fc99d18622ba4b23a678c57ccb6cb66862
Raj Dubey <com.mailuser@gmail.com>
2022-10-28 21:32:21 +0530

    added ping to api version at bottom of page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UploadService } from './service/upload.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'datasaver-frontEnd';
+
+  service_status:string='';
+constructor(private uservice:UploadService){
+
+}
+  ngOnInit(): void {
+    console.log("pinging!!!")
+    
+    let res=this.uservice.ping().subscribe(data=>{
+      console.log(data)
+
+      this.service_status=data.message;
+    });
+  }
+
+
 }
