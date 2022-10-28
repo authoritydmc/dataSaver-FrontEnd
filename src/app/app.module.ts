8128a708ce8b9114b1e4feeea33711d0d6e79b10
Raj Dubey <com.mailuser@gmail.com>
2022-10-28 19:18:31 +0530

    upload form made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UploadComponent } from './components/upload/upload.component';
+import { ViewFileComponent } from './components/view-file/view-file.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    ViewFileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
